2d82c6baf2065a88d2a09f4f3c547d7df19e79f4
Lorenzo Villani <lorenzo@villani.me>
2013-05-20 18:23:26 +0200

    Add fish configuration file

---
+++ config/fish/config.fish
@@ -0,0 +1,10 @@
+set -e fish_greeting
+set -x DEBEMAIL="lorenzo@villani.me"
+set -x DEBFULLNAME="Lorenzo Villani"
+set -x EDITOR subl
+set -x GEM_HOME $HOME/.gem/ruby/1.9.1
+set -x GIT_EDITOR nano
+set -x PATH ~/.local/bin $GEM_HOME/bin $PATH
+
+alias e subl
+alias g git
